461fc71b461ee13d250ec4ae37ed15ba20730a3b
Bladepool <100499358+CFG-NINJA@users.noreply.github.com>
2023-02-21 00:05:50 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,7 @@ export const BUNDLE_ID = '1'
 export const timeframeOptions = {
   WEEK: '1 week',
   MONTH: '1 month',
-  // THREE_MONTHS: '3 months',
+  THREE_MONTHS: '3 months',
   // YEAR: '1 year',
   ALL_TIME: 'All time'
 }
